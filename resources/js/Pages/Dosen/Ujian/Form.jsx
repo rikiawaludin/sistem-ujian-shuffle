4ffc502cd5ed0329750327b11f0d5cf7b840b880
rikiawaludin <rikiawaludin17@gmail.com>
2025-06-15 01:51:07 +0700

    UI Pick ujian

--- resources/js/Pages/Dosen/Ujian/Form.jsx
+++ resources/js/Pages/Dosen/Ujian/Form.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, useForm, Link } from '@inertiajs/react';
 import { Button, Card, Input, Select, Option, Textarea, Checkbox, Typography } from '@material-tailwind/react';
+import { Editor } from '@tinymce/tinymce-react';
 
 export default function Form({ ujian, mataKuliahOptions }) {
     const isEditMode = !!ujian;
@@ -50,15 +51,73 @@ export default function Form({ ujian, mataKuliahOptions }) {
                                 </Typography>
                             )}
                         </div>
-                        <Textarea label="Deskripsi (Opsional)" value={data.deskripsi} onChange={e => setData('deskripsi', e.target.value)} />
-                        <div></div> {/* Spacer */}
-                        <Input type="number" label="Durasi (menit)" value={data.durasi} onChange={e => setData('durasi', e.target.value)} error={!!errors.durasi} />
-                        <Input type="number" label="KKM" value={data.kkm} onChange={e => setData('kkm', e.target.value)} error={!!errors.kkm} />
-                        <Input type="datetime-local" label="Waktu Mulai" value={data.tanggal_mulai} onChange={e => setData('tanggal_mulai', e.target.value)} error={!!errors.tanggal_mulai} />
-                        <Input type="datetime-local" label="Waktu Selesai" value={data.tanggal_selesai} onChange={e => setData('tanggal_selesai', e.target.value)} error={!!errors.tanggal_selesai} />
-                        <Checkbox label="Acak Soal?" checked={data.acak_soal} onChange={e => setData('acak_soal', e.target.checked)} />
-                        <Checkbox label="Tampilkan Hasil ke Siswa?" checked={data.tampilkan_hasil} onChange={e => setData('tampilkan_hasil', e.target.checked)} />
                     </div>
+                    {/* Deskripsi Editor - Full Width */}
+                    <div className="mt-6 mb-6">
+                        <Typography variant="h6" color="blue-gray" className="mb-2">
+                            Deskripsi (Opsional)
+                        </Typography>
+                        <Editor
+                            apiKey="oatu6jzb2f3zggwf9ja9c5njnil27bsbiyvc3ow0j5ersbt4"
+                            value={data.deskripsi}
+                            onEditorChange={(content) => setData('deskripsi', content)}
+                            init={{
+                                height: 200,
+                                menubar: false,
+                                plugins:
+                                    'lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help wordcount',
+                                toolbar:
+                                    'undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+                            }}
+                        />
+                        {errors.deskripsi && (
+                            <Typography color="red" className="mt-1 text-sm">
+                                {errors.deskripsi}
+                            </Typography>
+                        )}
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <Input
+                            type="datetime-local"
+                            label="Waktu Mulai"
+                            value={data.tanggal_mulai}
+                            onChange={e => setData('tanggal_mulai', e.target.value)}
+                            error={!!errors.tanggal_mulai}
+                        />
+                        <Input
+                            type="datetime-local"
+                            label="Waktu Selesai"
+                            value={data.tanggal_selesai}
+                            onChange={e => setData('tanggal_selesai', e.target.value)}
+                            error={!!errors.tanggal_selesai}
+                        />
+                        <Input
+                            type="number"
+                            label="Durasi (menit)"
+                            value={data.durasi}
+                            onChange={e => setData('durasi', e.target.value)}
+                            error={!!errors.durasi}
+                        />
+                        <Input
+                            type="number"
+                            label="KKM"
+                            value={data.kkm}
+                            onChange={e => setData('kkm', e.target.value)}
+                            error={!!errors.kkm}
+                        />
+                        <Checkbox
+                            label="Acak Soal?"
+                            checked={data.acak_soal}
+                            onChange={e => setData('acak_soal', e.target.checked)}
+                        />
+                        <Checkbox
+                            label="Tampilkan Hasil ke Siswa?"
+                            checked={data.tampilkan_hasil}
+                            onChange={e => setData('tampilkan_hasil', e.target.checked)}
+                        />
+                    </div>
+
                     <div className="flex justify-end mt-6 gap-3">
                         <Link href={route('dosen.ujian.index')}><Button variant="text">Batal</Button></Link>
                         <Button type="submit" disabled={processing}>{isEditMode ? 'Simpan Perubahan' : 'Lanjutkan & Tambah Soal'}</Button>
